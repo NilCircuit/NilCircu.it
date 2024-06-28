1be070e90fc2e0a29bdcc7b0ebece8a120e987c9
Teptonic <69185285+Teptonic@users.noreply.github.com>
2024-06-28 11:04:51 -0600

    w
    w

--- scripts/binds.js
+++ scripts/binds.js
@@ -8,16 +8,16 @@ let ipToggled = false;
 let dateToggled = false;
 
 let onColorTxtShadow = `0 0 5px rgb(0, 255, 21), 0 0 10px rgb(0, 255, 21), 0 0 20px rgb(0, 255, 21)`;
-let onColorTxtColor = `rgb(0, 255, 21)`;
-
 let offColorTxtShadow = `0 0 5px rgb(255, 79, 79), 0 0 10px rgb(255, 79, 79), 0 0 20px rgb(255, 79, 79)`;
+
+let onColorTxtColor = `rgb(0, 255, 21)`;
 let offColorTxtColor = 'rgb(255, 0, 0)';
 
 let onColorButtonShadow = `0 0 5px rgb(79, 255, 79), 0 0 10px rgb(79, 255, 79), 0 0 20px rgb(79, 255, 79)`;
 let offColorButtonShadow = `0 0 5px rgb(255, 79, 79), 0 0 10px rgb(255, 79, 79), 0 0 20px rgb(255, 79, 79)`;
 
-let onColorButtonColor = `rgba(0, 255, 0, 0.5)`;
-let offColorButtonColor = `rgba(255, 0, 0, 0.5))`;
+let onColorButtonColor = `rgba(0, 255, 0)`;
+let offColorButtonColor = `rgba(255, 0, 0)`;
 
 
 function toggleIP(){
@@ -25,16 +25,16 @@ function toggleIP(){
         // Slide off of screen
         animate.slideOnScreenRight(ipContainer, -500, 750)
         ipToggleBtn.style.boxShadow = offColorButtonShadow;
-        ipToggleBtn.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+        ipToggleBtn.style.backgroundColor = offColorButtonColor;
         ipToggleTxt.style.color = offColorTxtColor;
-        ipToggleTxt.style.textShadow = '0 0 5px rgb(255, 79, 79), 0 0 10px rgb(255, 79, 79), 0 0 20px rgb(255, 79, 79)';
+        ipToggleTxt.style.textShadow = offColorTxtShadow;
     } else if (ipToggled == true) {
         // Slide onto screen
         animate.slideOffScreenRight(ipContainer, 500, 750)
-        ipToggleBtn.style.boxShadow = '0 0 5px rgb(79, 255, 79), 0 0 10px rgb(79, 255, 79), 0 0 20px rgb(79, 255, 79)';
-        ipToggleBtn.style.backgroundColor = 'rgba(0, 255, 0, 0.5)';
-        ipToggleTxt.style.color = 'rgb(0, 255, 21)';
-        ipToggleTxt.style.textShadow = '0 0 5px rgb(0, 255, 21), 0 0 10px rgb(0, 255, 21), 0 0 20px rgb(0, 255, 21)';
+        ipToggleBtn.style.boxShadow = onColorButtonShadow;
+        ipToggleBtn.style.backgroundColor = onColorButtonColor;
+        ipToggleTxt.style.color = onColorTxtColor;
+        ipToggleTxt.style.textShadow = onColorTxtShadow;
     }
     ipToggled = !ipToggled
 }
@@ -43,13 +43,17 @@ function toggleDate(){
     if (dateToggled == false) {
         // Slide off of screen
         animate.slideOnScreenRight(timeContainer, -500, 750)
-        dateToggleBtn.style.boxShadow = '0 0 5px rgb(255, 79, 79), 0 0 10px rgb(255, 79, 79), 0 0 20px rgb(255, 79, 79)';
-        dateToggleBtn.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+        dateToggleBtn.style.boxShadow = offColorButtonShadow;
+        dateToggleBtn.style.backgroundColor = offColorButtonColor;
+        dateToggleTxt.style.color = offColorTxtColor;
+        dateToggleTxt.style.textShadow = offColorTxtShadow;
     } else if (dateToggled == true) {
         // Slide onto screen
         animate.slideOffScreenRight(timeContainer, 500, 750)
-        dateToggleBtn.style.boxShadow = '0 0 5px rgb(79, 255, 79), 0 0 10px rgb(79, 255, 79), 0 0 20px rgb(79, 255, 79)';
-        dateToggleBtn.style.backgroundColor = 'rgba(0, 255, 0, 0.5)';
+        dateToggleBtn.style.boxShadow = onColorButtonShadow;
+        dateToggleBtn.style.backgroundColor = onColorButtonColor;
+        dateToggleTxt.style.color = onColorTxtColor;
+        dateToggleTxt.style.textShadow = onColorTxtShadow;
     }
     dateToggled = !dateToggled
 }
